30ce9b1f26fbac0d34eeed77f9eb462396d74be4
Pooja2111 <60030850+Pooja2111@users.noreply.github.com>
2020-01-25 18:12:10 +0530

    Add files via upload

---
+++ Automobile.java
@@ -0,0 +1,23 @@
+package Mobile;
+
+class Car {
+	int carnumber;
+	int run()
+	{
+		return 50;
+	}
+}
+class Fordcar extends Car
+{
+	int run()
+	{
+		return super.run()+40;
+	}
+}
+class Automobile
+{
+	public static void main(String args[]) {
+     Fordcar f=new Fordcar();
+     System.out.println(f.run());
+	}
+}
